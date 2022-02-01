9b194c6be863f32f7d8b0678749db9d39b4e15df
alphek <alphekbitexen@gmail.com>
2022-02-01 23:38:39 +0300

    Add some commentation about assumtions

--- discount_svc/util.py
+++ discount_svc/util.py
@@ -4,6 +4,8 @@ import hashlib
 def generate_codes(brand_id, campaign_id, count):
     # to prevent anyone else generate a valid code, to further safety brand secret key can be used  to hash as well
     # code is currently too long must be changed with something more useful and shorter
+    # I could use uuid in db model but I could not be sure that this will create lock time over multiple brand create codes on the same time
+    # Further research needed
     codes = []
     for i in range(0, count):
         code_str = str(brand_id) + str(campaign_id) + str(i)
