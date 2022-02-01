ba6935ae1070b7df9b61418b3ab5c3ce9347eee8
alphek <alphekbitexen@gmail.com>
2022-02-01 16:27:39 +0300

    Fix indent of readme

--- discount_svc/util.py
+++ discount_svc/util.py
@@ -3,6 +3,7 @@ import hashlib
 
 def generate_codes(brand_id, campaign_id, count):
     # to prevent anyone else generate a valid code, to further safety brand secret key can be used  to hash as well
+    # code is currently too long must be changed with something more useful and shorter
     codes = []
     for i in range(0, count):
         code_str = str(brand_id) + str(campaign_id) + str(i)
